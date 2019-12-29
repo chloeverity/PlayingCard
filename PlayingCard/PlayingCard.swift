fcaf87bf9931c4764b9d3e2006dcc53b0bd2d87a
Chloe Verity <chloeverity@hotmail.co.uk>
2019-12-29 23:36:38 +0100

    Add PlayingCardView and update UI of each card

--- PlayingCard/PlayingCard.swift
+++ PlayingCard/PlayingCard.swift
@@ -8,20 +8,26 @@
 
 import Foundation
 
-struct PlayingCard {
+struct PlayingCard: CustomStringConvertible {
+    var description: String { return "\(suit)\(rank)" }
+    
     var suit: Suit
     var rank: Rank
     
-    enum Suit: String {
+    enum Suit: String, CustomStringConvertible {
+        
         case spades = "♠️"
         case hearts = "♥️"
         case diamonds = "♦️"
         case clubs = "♣️"
         
         static var all = [Suit.spades,.hearts,.diamonds,.clubs]
+        
+        var description: String { return rawValue }
     }
     
-    enum Rank {
+    enum Rank: CustomStringConvertible {
+        
         case ace
         case face(String)
         case numeric(Int)
@@ -44,6 +50,14 @@ struct PlayingCard {
             }
             allRanks += [Rank.face("J"),.face("Q"),.face("K")]
             return allRanks
+        }
+        
+        var description: String {
+           switch self {
+           case .ace: return "A"
+           case .numeric(let pips): return String(pips)
+           case .face(let kind): return kind
+           }
         }
     }
 }
